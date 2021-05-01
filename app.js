9ea9ae0a7018d911e95f9b8c3556747554b2682e
diegobaena89 <diegobf.cello@gmail.com>
2021-04-30 21:19:41 -0300

    add new features

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-const formAddInput = document.querySelector('.form-add-todo')
+const formAddTodo = document.querySelector('.form-add-todo')
+const ulInputGroup = document.querySelector('.todo-container')
+const eraseItem = document.querySelector('.fa-times')
 
-
-formAddInput.addEventListener('submit', event => {
+formAddTodo.addEventListener('submit', event => {
   event.preventDefault()
+
+  const inputValue = event.target.add.value.trim();
+  if(inputValue.length) {
+
+    ulInputGroup.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${inputValue}</span>
+    <i class="fas fa-times"></i>
+    </li>
+    `
+    event.target.reset()
+  }
+})
+
+
+ulInputGroup.addEventListener('click', event => {
+  const clicked = event.target
+
+  console.log(Array.from(clicked.classList).includes('delete'))
+
 })
