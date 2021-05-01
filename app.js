a7f7fe6f0f7c6e5e653e8784756e9728b06fd5d9
diegobaena89 <diegobf.cello@gmail.com>
2021-05-01 13:43:22 -0300

    add features on deleted items

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const ulInputGroup = document.querySelector('.todo-container')
 const eraseItem = document.querySelector('.fa-times')
+const formSearch = document.querySelector('.form-search input')
 
 formAddTodo.addEventListener('submit', event => {
   event.preventDefault()
@@ -11,17 +12,17 @@ formAddTodo.addEventListener('submit', event => {
     ulInputGroup.innerHTML += `
     <li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${inputValue}</span>
-    <i class="fas fa-times"></i>
+    <i class="fas fa-times delete"></i>
     </li>
     `
     event.target.reset()
   }
 })
 
-
 ulInputGroup.addEventListener('click', event => {
-  const clicked = event.target
-
-  console.log(Array.from(clicked.classList).includes('delete'))
-
+   const clickedElement = event.target
+  
+   if(Array.from(clickedElement.classList).includes('delete')){
+     clickedElement.parentElement.remove()
+   }
 })
