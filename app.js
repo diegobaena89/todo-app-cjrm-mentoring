bb7749d489fbb6cc12a82c7a758124b3b2978db5
diegobaena89 <diegobf.cello@gmail.com>
2021-05-04 21:33:12 -0300

    refactored

--- app.js
+++ app.js
@@ -7,46 +7,49 @@ const insertNewLis = event => {
   event.preventDefault()
 
   const inputValue = event.target.add.value.trim();
-  if(inputValue.length) {
 
+  if(inputValue.length) {
     ulInputGroup.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${inputValue}</span>
-    <i class="fas fa-times delete"></i>
-    </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="fas fa-times delete"></i>
+      </li>
     `
     event.target.reset()
   }
+
 }
-formAddTodo.addEventListener('submit', insertNewLis)
 
 const deleteClickedLis = event => {
   const clickedElement = event.target
   const arrayWithDeleteClass = Array.from(clickedElement.classList)
   .includes('delete')
-
+  
   if(arrayWithDeleteClass){
     clickedElement.parentElement.remove()
   }
+
 }
+
+formSearch.addEventListener('input', filteringInputValues)
+formAddTodo.addEventListener('submit', insertNewLis)
 ulInputGroup.addEventListener('click', deleteClickedLis)
 
 const filteringInputValues = event => {
   const inputSearchValue = event.target.value.trim().toLowerCase()
 
   Array.from(ulInputGroup.children)
-  .filter(task =>  !task.textContent.toLowerCase()
-  .includes(inputSearchValue))
+  .filter(task =>  !task.textContent.toLowerCase().includes(inputSearchValue))
   .forEach(task => {
+
     task.classList.remove('d-flex')
     task.classList.add('hidden')
   })
   Array.from(ulInputGroup.children)
-  .filter(task =>  task.textContent.toLowerCase()
-  .includes(inputSearchValue))
+  .filter(task =>  task.textContent.toLowerCase().includes(inputSearchValue))
   .forEach(task => {
+
     task.classList.remove('hidden')
     task.classList.add('d-flex')
   })
 }
-formSearch.addEventListener('input', filteringInputValues)
